ba0868c8df0b9fff10eac71e3f99d85996e795b9
Deepika <deepikaravindran1801@gmail.com>
2024-09-20 13:10:46 +0530

    any message

--- src/componens/Education.js
+++ src/componens/Education.js
@@ -8,7 +8,7 @@ export default function Education(){
 
             </div>
             <div className="flex justify-center py-5 flex-col md:flex-row gap-8" >
-                <div className="card-education w-[300px] md:w-[700px] my-3 px-3 text-center animation">
+                <div className="card-education  my-3 text-center animation">
                     <h1 className="font-bold text-white py-2 text-2xl hover:text-gray-400">Master of Computer Applications</h1>
                     <p className="text-gray-200 ">M.A.M College Of Engineering</p>
                     <p className="text-blue-300">2022 - 2024</p>
@@ -16,8 +16,8 @@ export default function Education(){
                     {/* <p>A Master of Computer Applications (MCA) is a postgraduate degree designed to provide in-depth knowledge of computer science and its practical applications. It covers advanced topics such as software development, programming, and systems management. The program equips students with skills needed for careers in IT and software engineering. Graduates are prepared for roles in technology development, analysis, and management.</p> */}
                 </div>
 
-                <div className="card-education  w-[300px] md:w-[700px] my-0 px-3 text-center">
-                    <h1 className="font-bold text-blue-950 text-white py-2 text-2xl hover:text-gray-400">Bachelor of Computer Applications</h1>
+                <div className="card-education   my-0 px-7 text-center">
+                    <h1 className="font-bold text-white py-2 text-2xl hover:text-gray-400">Bachelor of Computer Applications</h1>
                     <p className="text-gray-400 ">Srimad Andavan Arts and Science College</p>
                     <p className="text-blue-300 ">2019 - 2022</p>
                     <p className="text-white py-2"><span className="text-blue-300">Grade:</span>  8.9 CGPA</p>
